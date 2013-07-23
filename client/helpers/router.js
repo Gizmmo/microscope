cae81709353db640685f273c453390b68201b5a7
Gizmmo <travisscott301@gmail.com>
2013-07-22 19:10:05 -0500

    Added a submit post page, and linked it to the header

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,11 +1,8 @@
 Meteor.Router.add({
-    '/': 'postsList'
-});
-
-Meteor.Router.add({
     '/': 'postsList',
     '/posts/:_id': {
         to: 'postPage',
         and: function(id) { Session.set('currentPostId', id); }
-    }
+    },
+    '/submit': 'postSubmit'
 });
