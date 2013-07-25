d9d035840585809053ac8947b1b96bd2280f7eae
Gizmmo <travisscott301@gmail.com>
2013-07-24 19:54:07 -0500

    Basic Error Reporting

---
+++ client/helpers/errors.js
@@ -0,0 +1,11 @@
+// Local (client-only) collection
+Errors = new Meteor.Collection(null);
+
+throwError = function(message) {
+    Errors.insert({message: message})
+}
+Template.errors.helpers({
+    errors: function() {
+        return Errors.find();
+    }
+});
